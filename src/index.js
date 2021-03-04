e10e6a9a2c093e3cf801870a0b928790e410fe63
brynbaxter <brynmcivor1@gmail.com>
2021-03-04 14:36:38 +1300

    rework CSS

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     let dictionaryFile = this.responseText;
     const wordArr = dictionaryFile.replace(/\r/g, '').split('\n');
-    fourLetterPlusArr = wordArr.filter((word) => word.length >= 4);
+    fourLetterPlusArr = wordArr.filter(word => word.length >= 4);
 
     // arrange words in ascending length
     fourLetterPlusArr.sort((a, b) => {
@@ -24,9 +24,10 @@ xmlhttp.send();
 const form = document.querySelector('.wordSearch');
 const resultsObj = document.querySelector('#resultList');
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', e => {
   e.preventDefault();
   resultsObj.innerHTML = '';
+  console.log('this worked!');
   let table = form.tableWord.value; // str
   let hand = form.userHand.value.split(''); // char array
   let minWordLength = form.minLength.checked ? 5 : 4; // 4 or 5
